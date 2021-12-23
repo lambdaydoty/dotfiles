c287fd626f991e0e0c6f27d18a5a369e9a52f3bb
lambdaydoty <euphrates.tigris@gmail.com>
2021-12-23 03:34:00 +0000

    Enhance install-nvim.sh

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 ver=v0.6.0
+loc=$HOME/.local/bin
 
 curl -LJO "https://github.com/neovim/neovim/releases/download/${ver}/nvim.appimage" \
   && curl -LJO "https://github.com/neovim/neovim/releases/download/${ver}/nvim.appimage.sha256sum"
@@ -8,4 +9,8 @@ curl -LJO "https://github.com/neovim/neovim/releases/download/${ver}/nvim.appima
 sha256sum -c nvim.appimage.sha256sum
 chmod u+x ./nvim.appimage
 ./nvim.appimage --appimage-extract
-ln -s "$(pwd)/squashfs-root/usr/bin/nvim" "$HOME/.local/bin/nvim"
+
+[ -d $loc ] || mkdir -p "$loc"
+
+ln -s "$(pwd)/squashfs-root/usr/bin/nvim" "$loc/nvim"
+ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
